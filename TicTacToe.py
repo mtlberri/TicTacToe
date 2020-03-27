a0b651337df331edabebdcb241a36bf7caa50bc3
joffrey <joffrey.armellini@gmail.com>
2020-03-27 00:18:38 -0400

    first MVP version of the project

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,8 +1,28 @@
 # Tic Tac Toe game
 
-# Initialize
-my_player_X_moves = [True,False,False,False,False,True,False,False,False]
-my_player_O_moves = [False,True,False,False,False,False,False,False,False]
+# Initialize lists to represent the moves of each player
+player_X_moves = [False]*9
+player_O_moves = [False]*9
+
+# Create a string to manage which of the players (X or O) is to play. Start with X.
+current_player = 'X'
+
+def get_player_move():
+    '''Function that gets player input and update his/her moves accordingly'''
+    player_input_str = input(f"Player {current_player}:")
+    ## Try to cast the player input into an int
+    try:
+        player_input = int(player_input_str)
+        if player_input in range(1,10):
+            print(f"valid input: {player_input}")
+            if current_player == 'X':
+                player_X_moves[player_input-1] = True
+            else:
+                player_O_moves[player_input-1] = True
+        else:
+            print(f"invalid input: {player_input_str}")
+    except:
+        print(f"invalid input: {player_input_str}")
 
 def which_symbol(move_X,move_O):
     '''Functiun that returns the symbol to be displayed in a given board location'''
@@ -13,11 +33,10 @@ def which_symbol(move_X,move_O):
     else:
         return ' '
 
-def display_board(player_X_moves,player_O_moves):
+def display_board():
     '''Function that displays the Tic Tac Toe Board based on users moves'''
-    board_data = [which_symbol(tup[0], tup[1]) for tup in zip(player_X_moves, player_O_moves)]
     # Create a list that will hold X or O in each position of the board
-    board_data = [which_symbol(tup[0], tup[1]) for tup in zip(my_player_X_moves, my_player_O_moves)]
+    board_data = [which_symbol(tup[0], tup[1]) for tup in zip(player_X_moves, player_O_moves)]
     # Create the board based on its data
     board = f"{board_data[6]}|{board_data[7]}|{board_data[8]}\n" \
             f"{board_data[3]}|{board_data[4]}|{board_data[5]}\n" \
@@ -25,4 +44,22 @@ def display_board(player_X_moves,player_O_moves):
     # Print out the board
     print(board)
 
-display_board(my_player_X_moves,my_player_O_moves)
+def alternate_player(player):
+    if player == 'X':
+        player = 'O'
+    else:
+        player = 'X'
+    return player
+
+for i in range(0,3):
+    # Get player move
+    get_player_move()
+    # Display the board
+    display_board()
+    # Check if game is won, lost or null
+    # If that game is either won, lost or null: display a message and propose to continue playing or quit
+    # Alternate user to play next
+    current_player = alternate_player(current_player)
+
+
+
